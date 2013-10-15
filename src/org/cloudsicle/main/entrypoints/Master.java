2bd218c7962dfce84974cd5faf27e233e3c3fb49
ChrisTitos <chris_titos@hotmail.com>
2013-10-15 12:35:35 +0200

    - Started work on the Scheduler. We're starting with a very rough version just so we can see something working. - Added some attributes to JobMetaData

--- src/org/cloudsicle/main/entrypoints/Master.java
+++ src/org/cloudsicle/main/entrypoints/Master.java
@@ -1,28 +1,41 @@
 package org.cloudsicle.main.entrypoints;
 
+import java.io.IOException;
+
 import org.cloudsicle.communication.IMessageHandler;
+import org.cloudsicle.communication.SocketListener;
 import org.cloudsicle.master.Scheduler;
 import org.cloudsicle.messages.IMessage;
+import org.cloudsicle.messages.JobMetaData;
 
 public class Master implements IMessageHandler {
 
 	private Scheduler scheduler;
+	private SocketListener listener;
+	
+	private final static int port = 1;
 
-	public Master() {
+	public Master() throws Exception {
 		this.scheduler = new Scheduler();
+		this.listener = new SocketListener(this, port);
 	}
-
+	
 	@Override
-	public void process(IMessage message) {
-		// TODO Auto-generated method stub
-
+	public void process(IMessage message) {		
+		if(message instanceof JobMetaData){
+			this.scheduler.schedule((JobMetaData) message);
+		}
 	}
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-
+		try {
+			Master master = new Master();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 }
--- src/org/cloudsicle/messages/JobMetaData.java
+++ src/org/cloudsicle/messages/JobMetaData.java
@@ -1,5 +1,52 @@
 package org.cloudsicle.messages;
 
+import java.net.InetAddress;
+
+/**
+ * The JobMetaData is presented to the master by the client so that the master can allocate
+ * VM(s) for the actual job
+ */
 public class JobMetaData implements IMessage {
+
+	private static final long serialVersionUID = 4966126383441403840L;
+	
+	private InetAddress client;
+	private int totalFileSize;
+	private int numFiles;
+	
+	/**
+	 * 
+	 * @param c The InitAddress of the client
+	 */
+	public JobMetaData(InetAddress c){
+		
+	}
+	
+	public InetAddress getClient() {
+		return client;
+	}
+
+	public void setClient(InetAddress client) {
+		this.client = client;
+	}
+
+	public int getTotalFileSize() {
+		return totalFileSize;
+	}
+
+	public void setTotalFileSize(int totalFileSize) {
+		this.totalFileSize = totalFileSize;
+	}
+
+	public int getNumFiles() {
+		return numFiles;
+	}
+
+	public void setNumFiles(int numFiles) {
+		this.numFiles = numFiles;
+	}
+
+
+	
 
 }
