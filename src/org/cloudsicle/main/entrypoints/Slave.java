f2e81e90f9db31f2d1525c406fd81c2af7905c54
qstokkink <goqs@hotmail.com>
2013-10-15 13:15:52 +0200

    Added gifsicle deployment on slave

--- src/org/cloudsicle/main/entrypoints/Slave.java
+++ src/org/cloudsicle/main/entrypoints/Slave.java
@@ -1,7 +1,34 @@
 package org.cloudsicle.main.entrypoints;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
 public class Slave {
 
+	/**
+	 * Initialize our Slave
+	 * 
+	 * @throws IOException If we failed to deploy gifsicle on the environment
+	 */
+	public Slave() throws IOException{
+		deployExecutable();
+	}
+	
+	/**
+	 * Copy our gifsicle executable from the jar to the environment
+	 */
+	private void deployExecutable() throws IOException{
+		InputStream is = Slave.class.getResourceAsStream("gifsicle");
+		FileOutputStream fos = new FileOutputStream(new File("~/gifsicle"));
+		while (is.available() > 0)
+			fos.write(is.read());
+		fos.flush();
+		fos.close();
+		is.close();
+	}
+	
 	/**
 	 * @param args
 	 */
