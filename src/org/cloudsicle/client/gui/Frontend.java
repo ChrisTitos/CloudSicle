1e64f6eba4c570c00e0b3ef8eb0ed5e8ed9c2ea1
qstokkink <goqs@hotmail.com>
2013-10-18 16:03:32 +0200

    Tied GUI to main entrypoint

--- src/org/cloudsicle/client/gui/Frontend.java
+++ src/org/cloudsicle/client/gui/Frontend.java
@@ -43,7 +43,7 @@ public class Frontend extends JFrame {
 	/**
 	 * Launch the application.
 	 */
-	public static void main(String[] args) {
+	public static void launch() {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
--- src/org/cloudsicle/main/entrypoints/Client.java
+++ src/org/cloudsicle/main/entrypoints/Client.java
@@ -1,13 +1,14 @@
 package org.cloudsicle.main.entrypoints;
 
+import org.cloudsicle.client.gui.Frontend;
+
 public class Client {
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Frontend.launch();
 	}
 
 }
