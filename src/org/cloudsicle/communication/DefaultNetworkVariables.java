672d5ce6309facc5e6a8982ec97b73824aa2ce88
ChrisTitos <chris_titos@hotmail.com>
2013-10-22 11:47:07 +0200

    port number back to 21007

--- src/org/cloudsicle/communication/DefaultNetworkVariables.java
+++ src/org/cloudsicle/communication/DefaultNetworkVariables.java
@@ -2,5 +2,5 @@ package org.cloudsicle.communication;
 
 public class DefaultNetworkVariables {
 
-	public static final int DEFAULT_PORT = 8080;
+	public static final int DEFAULT_PORT = 21007;
 }
--- src/org/cloudsicle/main/jobs/ProduceJob.java
+++ src/org/cloudsicle/main/jobs/ProduceJob.java
@@ -1,5 +1,43 @@
 package org.cloudsicle.main.jobs;
 
+import java.io.File;
+import java.util.ArrayList;
+
 public class ProduceJob implements IJob {
+	
+	private ArrayList<File> files;
+	private int delay;
+	private boolean loop;
+	
+	public ProduceJob(){
+		files = new ArrayList<File>();
+	}
+	
+	public void addFile(File f){
+		files.add(f);
+	}
+	
+	public void addFiles(ArrayList<File> files){
+		for(File f : files){
+			this.addFile(f);
+		}
+	}
+
+	public int getDelay() {
+		return delay;
+	}
+
+	public void setDelay(int delay) {
+		this.delay = delay;
+	}
+
+	public boolean isLoop() {
+		return loop;
+	}
+
+	public void setLoop(boolean loop) {
+		this.loop = loop;
+	}
+	
 
 }
